cd884e4d8562315384cb964947743de7df2d92a8
Demid Yermak <d.yermak@artjoker.net>
2022-09-18 21:47:11 +0300

    Add desktop and adaptive for page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+$(function () {
+  $('.js-accordion-btn').on('click', function () {
+    $(this).toggleClass('is-open').closest('.js-accordion').find('.js-accordion-item').slideToggle();
+  });
+});
