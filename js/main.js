c6d4b8611e07e4e3f7d2f25e4df0d886791c7779
Demid Yermak <d.yermak@artjoker.net>
2022-09-21 22:18:01 +0300

    add header adaptive

--- js/main.js
+++ js/main.js
@@ -5,4 +5,15 @@ $(function () {
     $(this).find('.js-accordion-item').slideToggle();
     $(this).find('.js-accordion-btn').toggleClass('is-open');
   });
+  $('.js-burger').on('click', function () {
+    $(this).toggleClass('is-active').siblings('.js-burger-modal').toggleClass('is-open');
+    $('body').toggleClass('no-scroll');
+    $('.js-overlay').toggleClass('is-visible');
+  });
+  $('.js-overlay').on('click', function () {
+    $(this).removeClass('is-visible');
+    $('.js-burger').removeClass('is-active');
+    $('.js-burger-modal').removeClass('is-open');
+    $('body').removeClass('no-scroll');
+  });
 });
